d786ef4f0c09bb7d7c3d2970413d7595f7baec7f
Mateusz Dobek <mat.dobek@gmail.com>
2024-12-05 04:53:05 +0100

    BasicExperience + modularize

--- course--threejs-journey/07--react-three-fiber/r3f/src/index.jsx
+++ course--threejs-journey/07--react-three-fiber/r3f/src/index.jsx
@@ -3,9 +3,11 @@ import { Canvas } from '@react-three/fiber'
 import './index.css'
 import { StrictMode } from 'react'
 import { Leva } from 'leva'
-// import Experience from './R3FExperience.jsx'
-// import Experience from './DreiExperience.jsx'
-import Experience from './DebugExperience.jsx'
+
+import Experience from './Components/Basic/Experience.jsx'
+// import Experience from './Components/R3F/Experience.jsx'
+// import Experience from './Components/Drei/Experience.jsx'
+// import Experience from './Components/Debug/Experience.jsx'
 
 const root = createRoot(document.querySelector('#root'))
 root.render(
